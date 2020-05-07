df6fdf9f77f86f6cc4c987db1e6ba5604e331ff7
nodamu <nickadamu@gmail.com>
2020-05-07 02:35:31 +0000

    Removed k8s  deploy config

--- deploy.sh
+++ deploy.sh
@@ -10,11 +10,4 @@ docker push nickadamu/multi-client:$SHA
 docker push nickadamu/multi-server:$SHA
 docker push nickadamu/multi-worker:$SHA
 
-openssl aes-256-cbc -K $encrypted_1ba1eb16049b_key -iv $encrypted_1ba1eb16049b_iv -in multi-k8s-kubeconfig.yaml.enc -out multi-k8s-kubeconfig.yaml -d
-
-kubectl --kubeconfig /home/travis/build/nodamu/multi-k8s/multi-k8s-kubeconfig.yaml apply -f k8s/
-
-kubectl  --kubeconfig /home/travis/build/nodamu/multi-k8s/multi-k8s-kubeconfig.yaml set image deployments/client-deployment client-deployment=nickadamu/multi-client:$SHA
-kubectl  --kubeconfig /home/travis/build/nodamu/multi-k8s/multi-k8s-kubeconfig.yaml set image deployments/server-deployment server-deployment=nickadamu/multi-server:$SHA
-kubectl  --kubeconfig /home/travis/build/nodamu/multi-k8s/multi-k8s-kubeconfig.yaml set image deployments/worker-deployment worker-deployment=nickadamu/multi-worker:$SHA
 
